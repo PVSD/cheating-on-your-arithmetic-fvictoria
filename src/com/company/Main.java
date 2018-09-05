9e11988c51401a62d9d2bbe874d7f4a36b509d6c
fvictoria <felipevictoria630@yahoo.com>
2018-09-05 16:36:45 -0400

    done

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,17 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int a = 79+3*(4+82-68)-7+19;
+        String b = " 79 + 3 * (4 + 82 - 68) - 7 + 1 ";
+        System.out.println(b+ " = " +a);
+
+        int i = (179+21+10)/7+181;
+        String j = "(179 + 21 + 10) / 7 + 181";
+        System.out.println(j+ " = " +i);
+
+        int x = 10389*56*11+2246;
+        String y = " 10389 * 56 * 11 + 2246 ";
+        System.out.println(y+ " = " +x);
+
     }
 }
